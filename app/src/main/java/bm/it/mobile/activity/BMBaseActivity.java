27a1b19aa2f6bfdc07d3ea762b69b1f9b83df16f
Thales Bertolini <thales.bm92@gmail.com>
2017-06-10 18:09:49 +0100

    toolbar + themes

--- app/src/main/java/bm/it/mobile/activity/BMBaseActivity.java
+++ app/src/main/java/bm/it/mobile/activity/BMBaseActivity.java
@@ -3,13 +3,37 @@ package bm.it.mobile.activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
 public class BMBaseActivity extends AppCompatActivity {
 
-    public View getView() {
-        return findViewById(android.R.id.content);
+    private Toolbar toolbar;
+
+    protected void setupToolbar(View toolbarView) {
+        toolbar = (Toolbar) toolbarView;
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    protected void changeToolbarTitle(String title) {
+        if (toolbar != null) {
+            toolbar.setTitle(title);
+        }
+    }
+
+    protected void setToolbarProperties(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    protected Toolbar getToolbar() {
+        return toolbar;
     }
 
     @Override
@@ -24,5 +48,13 @@ public class BMBaseActivity extends AppCompatActivity {
     public static boolean isOnline(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isAvailable() && cm.getActiveNetworkInfo().isConnected();
+    }
+
+    /**
+     * Return the screen view
+     * @return view
+     */
+    public View getView() {
+        return findViewById(android.R.id.content);
     }
 }
